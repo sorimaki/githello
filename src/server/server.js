2f63882599bdf7c40db1e2a88d8b9cb9398a79e3
mihai <sorinsystems@gmail.com>
2020-05-28 13:30:32 -0400

    bundle.js

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+import path from 'path';
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+let port = 7777;
+let app = express ();
+
+ app.listen(port, console.log(`Server listening on port ${port}`));
+//  app.get('/', (req, res) => {
+//      res.send("Hello World from server.js");
+//  })
+
+
+ app.use(express.static(path.resolve(__dirname,'../../dist')));
+ app.get('/*',(req,res)=>{
+     res.sendFile(path.resolve('index.html'));
+ });
